cd35ebbad897ae2c07090f01ddb14879ec5e7178
MeharGunesh <satyagunesh5418@gmail.com>
2023-11-29 12:16:56 +0530

    changes made on src folder  navbar and fotter components for responsive and updatetd link added in public folder in index.html

--- client/src/components/fotter/fotter.js
+++ client/src/components/fotter/fotter.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./fotter.css"
 const fotter = () => {
   return (
-    <div className='container-fluid p-3 d-flex justify-content-center align-items-center text-dark fotter'>
+    <div className='container-fluid fixed-bottom p-3 d-flex justify-content-center align-items-center text-dark fotter'>
         <h4>Todo</h4> &nbsp; <p className='m-0'>&copy;codingSkill</p>
         </div>
   )
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -21,14 +21,14 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-secondary">
+            <nav className="navbar fixed-top navbar-expand-lg bg-secondary">
                 <div className="container">
                     <Link className="navbar-brand text-white" to="/">
                         <b>
                             <GiWhiteBook /> &nbsp; TODO
                         </b>
                     </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler btn-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
